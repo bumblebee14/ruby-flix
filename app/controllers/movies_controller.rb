62735ccffe46d4f86caca4c3cfdca6800ac63fd9
bumblebee14 <sardar.umar1@gmail.com>
2024-01-08 14:56:24 -0500

    Edit, create and partials

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,25 @@ class MoviesController < ApplicationController
     end
 
     def update
-        fail
         @movie = Movie.find(params[:id])
+        @movie.update(movie_params)
+        redirect_to @movie
     end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        @movie.save
+        redirect_to @movie
+    end
+
+private
+
+    def movie_params
+        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    end
+
 end
